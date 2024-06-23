d2d671bb0209eaf5f9a3c4672bdd5b705f7555cd
Lindsey Reams <reamslin@gmail.com>
2024-06-22 20:09:53 -0400

    test.rkt

--- test.rkt
+++ test.rkt
@@ -2,58 +2,60 @@
 
 setlists {
   1 {
-  date { 05 08 77 }
-  venue {
-    name { Barton Hall }
-    city { Ithaca }
-    state { NY }
-  },
-  songs {
-    Morning Dew 
-    Cassidy {
-       Guitar Intro
-    }
-    Scarlet Begonias 
-    Fire on the Mountain 
-    St. Stephen {
-      Guitar Intro
-    }
-    Not Fade Away {
-      Drum Intro
-    }
-  }},
-  2 {
-  date { 06 18 74 }
-  venue {
-    name { Freedom Hall }
-    city { Louisville }
-    state { KY }
-  }
-  songs {
-    Eyes of the World 
-    China Cat Sunflower 
-    I Know You Rider 
-    Dark Star {
-      Dark Star Intro Riff
-      Mixo-Penta jamming
-      Shall We Go Motiff
-      Mixo-Penta jamming
-      Breakdown {
-        guitar { trippy trilling }
-        bass { riffing }
+    date { 05 08 77 }
+    venue {
+      name { Barton Hall }
+      city { Ithaca }
+      state { NY }
+    },
+    songs {
+      Morning Dew 
+      Cassidy {
+         Guitar Intro
       }
-      Mixo-Penta buildup
-      Mixo-Penta jamming
-      Minor break
-      Mixo-Penta jamming
-      Dark Star Riff
-      Mixo-Penta jamming
+      Scarlet Begonias 
+      Fire on the Mountain 
+      St. Stephen {
+       Guitar Intro
+      }
+      Not Fade Away {
+        Drum Intro
+      }
     }
-    Playing in the Band 
-    Ship of Fools {
-      Piano Intro 
+  },
+  2 {
+    date { 06 18 74 }
+    venue {
+      name { Freedom Hall }
+      city { Louisville }
+      state { KY }
     }
-  }},
+    songs {
+      Eyes of the World 
+      China Cat Sunflower 
+      I Know You Rider 
+      Dark Star {
+        Dark Star Intro Riff
+        Mixo-Penta jamming
+        Shall We Go Motiff
+        Mixo-Penta jamming
+        Breakdown {
+          guitar { trippy trilling }
+          bass { riffing }
+        }
+        Mixo-Penta buildup
+        Mixo-Penta jamming
+        Minor break
+        Mixo-Penta jamming
+        Dark Star Riff
+        Mixo-Penta jamming
+      }
+      Playing in the Band 
+      Ship of Fools {
+        Piano Intro 
+      }
+    }
+  },
   3 {
     date { 12 31 78 }
     venue {
